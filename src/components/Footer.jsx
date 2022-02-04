04fe9b15945a4c96187e17d9d43f676d2b8a1572
François Chatelier <francois.chatelier@gmail.com>
2022-02-04 11:51:46 +0100

    placement map sur le footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,6 +3,7 @@ import { LocationMarkerIcon } from "@heroicons/react/solid";
 import { Link } from "react-router-dom";
 import FooterData from "../data/FooterData";
 import ButtonAdmin from "./ButtonAdmin";
+import Map from "./Map";
 import { AuthContext } from "../contexts/authContext";
 
 /**
@@ -21,6 +22,7 @@ const Footer = () => {
       <div className="flex justify-end pr-4 pt-4">
         {authContext.token && <ButtonAdmin type="footer" />}
       </div>
+      <Map />
       <div className="py-4 flex flex-col items-center md:flex-row md:flex-wrap md:gap-0 text-center text-vert gap-5">
         <div className="md:order-1 md:basis-1/3 list-none">
           <h2 className="text-h2 font-bold">{FooterData.titreContact}</h2>

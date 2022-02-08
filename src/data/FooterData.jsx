d26afa98f372be76d80baf726bdbda8f97698690
maxime baillon <baillonmaximebaillon@gmail.com>
2022-02-08 10:59:52 +0100

    ajout des icons instagram, facebook et linkedin dans le footer

--- src/data/FooterData.jsx
+++ src/data/FooterData.jsx
@@ -4,6 +4,32 @@ const footer = {
   contact: [
     { id: 1, mail: "latrocquerie@gmail.com" },
     { id: 2, phone: "06 79 21 42 19" },
+    {
+      id: 3,
+      srcMailIcon: "./assets/icones/icon_mail.svg",
+    },
+    {
+      id: 4,
+      srcPhoneIcon: "./assets/icones/icon_phone.svg",
+    },
+    {
+      src: "./assets/icones/instagram.svg",
+      alt: "Instagram de la Trocquerie",
+      url: "https://www.instagram.com/latrocquerie/",
+      insta: "La Trocquerie",
+    },
+    {
+      src: "./assets/icones/iconFacebook.svg",
+      alt: "Facebook de la Trocquerie",
+      url: "https://www.facebook.com/latrocquerie.nantes",
+      facebook: "La Trocquerie",
+    },
+    {
+      src: "./assets/icones/iconLinkedin.svg",
+      alt: "LinkedIn de la Trocquerie",
+      url: "https://www.linkedin.com/company/latrocquerie/",
+      linkedin: "La Trocquerie",
+    },
   ],
   adresse: "23 rue Petite-Biesse - quartier île de Nantes",
   copyright: "© 2022 LaTrocquerie - Tous droits réservés",

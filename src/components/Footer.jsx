fa6220edeefda954f49a3af6ce761aebfcc0d898
Victor Pagnier <victor.pagnier@gmail.com>
2022-02-08 12:22:40 +0100

    Formulaire actif et relié avec le back

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,5 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
+import axios from "axios";
 import { LocationMarkerIcon } from "@heroicons/react/solid";
 import { Link } from "react-router-dom";
 import FooterData from "../data/FooterData";
@@ -17,12 +18,108 @@ import { AuthContext } from "../contexts/authContext";
 const Footer = () => {
   const authContext = useContext(AuthContext);
 
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [subject, setSubject] = useState("");
+  const [message, setMessage] = useState("");
+  const [alert, setAlert] = useState(null);
+
+  const resetForm = () => {
+    setName("");
+    setEmail("");
+    setSubject("");
+    setMessage("");
+  };
+
+  const submitEmail = (event) => {
+    event.preventDefault();
+    axios({
+      method: "POST",
+      url: "http://localhost:8000/api/email/send",
+      data: {
+        name,
+        email,
+        subject,
+        message,
+      },
+    }).then((response) => {
+      if (response.data.status === "success") {
+        setAlert(
+          <span className="text-lime-600">
+            Votre message a bien été envoyé!
+          </span>
+        );
+        resetForm();
+      } else if (response.data.status === "fail") {
+        setAlert(
+          <span className="text-red-600">
+            Lenvoi du message a échoué! (erreur serveur)
+          </span>
+        );
+      }
+    });
+  };
+
   return (
     <div className={FooterData.cls}>
       <div className="flex justify-end pr-4 pt-4">
         {authContext.token && <ButtonAdmin type="footer" />}
       </div>
       <Map />
+      <div className="flex flex-col items-center border-b">
+        <form id="contact-form" onSubmit={submitEmail} method="POST">
+          <div>
+            <input
+              className="border-2 border-vert rounded-md p-2 m-2 focus:outline-none focus:border-rose sm:pr-96"
+              placeholder="Name"
+              id="name"
+              type="text"
+              required
+              value={name}
+              onChange={(event) => setName(event.target.value)}
+            />
+          </div>
+          <div>
+            <input
+              className="border-2 border-vert rounded-md p-2 m-2 focus:outline-none focus:border-rose sm:pr-96"
+              placeholder="Email"
+              id="email"
+              type="email"
+              required
+              value={email}
+              onChange={(event) => setEmail(event.target.value)}
+            />
+          </div>
+          <div>
+            <input
+              className="border-2 border-vert rounded-md p-2 m-2 focus:outline-none focus:border-rose sm:pr-96"
+              placeholder="Subject"
+              id="subject"
+              type="text"
+              required
+              value={subject}
+              onChange={(event) => setSubject(event.target.value)}
+            />
+          </div>
+          <div>
+            <textarea
+              className="border-2 border-vert rounded-md p-2 m-2 focus:outline-none focus:border-rose sm:pr-96"
+              placeholder="Message"
+              id="message"
+              required
+              value={message}
+              onChange={(event) => setMessage(event.target.value)}
+            />
+          </div>
+          <button
+            className="text-white bg-vert p-2 px-16 m-2 rounded-md sm:px-64"
+            type="submit"
+          >
+            Submit
+          </button>
+          {alert && <p>{alert}</p>}
+        </form>
+      </div>
       <div className="py-4 flex flex-col items-center md:flex-row md:flex-wrap md:gap-0 text-center text-vert gap-5">
         <div className="md:order-1 md:basis-1/3 list-none">
           <h2 className="text-h2 font-bold">{FooterData.titreContact}</h2>

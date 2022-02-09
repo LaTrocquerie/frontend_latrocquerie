7a5ab7a0c99e15fa909e3159da042f1e2db51397
François Chatelier <francois.chatelier@gmail.com>
2022-02-09 17:11:01 +0100

    changement donnée footer

--- src/data/FooterData.jsx
+++ src/data/FooterData.jsx
@@ -3,7 +3,7 @@ const footer = {
   titreContact: "Contact",
   contact: [
     { id: 1, mail: "latrocquerie@gmail.com" },
-    { id: 2, phone: "06 79 21 42 19" },
+    { id: 2, phone: "06 00 00 00 00" },
     {
       id: 3,
       srcMailIcon: "./assets/icones/icon_mail.svg",
